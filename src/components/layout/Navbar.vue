5127dda4f704d6b919b04efbbb8dfe9159588232
yajared <jaredc32@gmail.com>
2024-03-20 14:08:13 -0600

    add some real pages, user creation, login, logout, authentication and tokens

---
+++ src/components/layout/Navbar.vue
@@ -0,0 +1,32 @@
+<template>
+  <nav class="navbar is-dark">
+    <div class="navbar-brand">
+      <router-link to="/" class="navbar-item">
+          <strong>Ganar CRM</strong>
+      </router-link>
+    </div>
+
+    <div class="navbar-menu">
+      <div class="navbar-end">
+          <div class="navbar-items">
+              <div class="buttons">
+                  <router-link to="/sign-up" class="button is-success"><strong>Sign up</strong></router-link>
+                  <router-link to="/log-in" class="button is-light">Log in</router-link>
+              </div>
+          </div>
+      </div>
+    </div>
+  </nav>
+</template>
+
+<style lang="scss">
+  .buttons {
+    margin: 8px 12px 0 12px !important;
+  }
+</style>
+
+<script>
+  export default {
+      name: 'Navbar'
+  }
+</script>
---
+++ src/views/dashboard/MyAccount.vue
@@ -0,0 +1,38 @@
+<template>
+  <div class="container">
+    <div class="column is-multiline">
+      <div class="column is-12">
+        <h1 class="title">My Account</h1>
+      </div>
+
+      <div class="column is-12">
+        <button @click="logout()" class="button is-danger">Log out</button>
+      </div>
+    </div>
+  </div>
+</template>
+
+
+<script>
+  import axios from 'axios'
+
+  export default {
+    name: 'My Account',
+    methods: {
+      async logout() {
+        await axios
+          .post('/api/v1/token/logout/')
+          .then(response => {
+            console.log('Logged out');
+          })
+          .catch(error => {
+            console.log(JSON.stringify(error));
+          });
+
+        axios.defaults.headers.common['Authorization'] = '';
+        localStorage.removeItem('token');
+        this.$store.commit('removeToken');
+      }
+    }
+  }
+</script>
